9798acf986dc6e7eace18ce2fe86c6b717b528b7
foxvorism <imjohani2005@gmail.com>
2024-11-10 23:01:00 +0700

    feat: Create a relation with blog_pics

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -15,4 +15,9 @@ class Blog extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function blog_pics()
+    {
+        return $this->hasMany(BlogPic::class);
+    }
 }
