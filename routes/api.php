62ae67713858482d4137e46e430d60432f7944b6
foxvorism <imjohani2005@gmail.com>
2024-11-14 23:16:36 +0700

    fix: Fix get user info route error

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ Route::middleware('auth:sanctum')->prefix('auth')->group(function () {
     Route::prefix('users')->group(function () {
         Route::get('/', [UserController::class, 'index'])->name('user.index');
         Route::get('/{id}', [UserController::class, 'show'])->name('user.show');
-        Route::get('/info', [UserController::class, 'userInfo'])->name('user.info');
+        Route::get('/id/info', [UserController::class, 'userInfo'])->name('user.info');
         Route::post('/store', [UserController::class, 'store'])->name('user.store');
         Route::post('/{id}/update', [UserController::class, 'update'])->name('user.update');
         Route::delete('/{id}/destroy', [UserController::class, 'destroy'])->name('user.destroy');
